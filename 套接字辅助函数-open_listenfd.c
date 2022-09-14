573a59ba0c719c4fe192e875fb7efde772e9fc0d
qiqizheng <417542931@qq.com>
2022-09-15 00:01:45 +0800

    Update 套接字辅助函数-open_listenfd.c

--- 套接字辅助函数-open_listenfd.c
+++ 套接字辅助函数-open_listenfd.c
@@ -1,13 +1,23 @@
-/**open_listenfd函数打开并返回一个监听描述符，这个描述符在一个准备好的port上接收请求。**/
+/**open_listenfd函数创建一个监听描述符， 准备好接收连接请求。**/
 
 #include "csapp.h"
 int open_listenfd(char *port)
 {
- struct addrinfo hints, *listp, *p;
-  int listend, optval =1;
-  
+  int clientfd;
+  struct addrinfo hints, *listp, *p;
+ 
   memset(&hints, 0, sizeof(struct addrinfo));
   hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags    = AI_PASSIVE | AI_ADDRCONFIG;
+  hints.ai_flags    != AI_NUMERICSERV;
+ 
+  Getaddrinfo(NUll, port, &hints, &listp);
   
+  for(p=listp; p ; p=p->ai_next) {
+     if((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) continue;
+     
+     Setsockopt(listenfd, SQL_SOCKET, SO_REUEADDR, );
+  
+  }
   
 }
